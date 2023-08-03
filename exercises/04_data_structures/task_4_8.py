97d9e699a99fa6564ac07639d24c82f49d13a2ad
Evgenii <zekagorhakov@gmail.com>
2023-08-03 09:01:36 +0300

    Выполнено задание 4.8

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,19 @@
 """
 
 ip = "192.168.3.1"
+ip = ip.split('.')
+kod_list = []
+for i in ip:
+	i = int(i)
+	kod = []
+	while i != 1:
+		kod.append(str(i % 2))
+		i //= 2
+	kod.append('1')
+	kod.reverse()
+	while len(kod) != 8:
+		kod.insert(0, '0')
+	kod_list.append(''.join(kod))
+kod = kod_list
+print(ip[0].ljust(10) + ip[1].ljust(10) + ip[2].ljust(10) + ip[3])
+print(kod[0] +'  ' + kod[1] +'  ' + kod[2] +'  ' + kod[3])
