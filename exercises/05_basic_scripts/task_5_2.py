a38939b9803e75091032615ef5544b658dfbebb1
Evgenii <zekagorhakov@gmail.com>
2023-08-04 08:29:33 +0300

    Выполнено задание 5.2

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -30,3 +30,46 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+def dvoih(a):
+	a = str(bin(a)[2:])
+	while len(a) != 8:
+		a = '0' + a
+	return a
+
+
+def des(a):
+	a = list(a)
+	a.reverse()
+	des = 0
+	st = 0
+	for i in a:
+		if i == '0':
+			st += 1
+			continue
+		des += int(i) * 2 ** st
+		st += 1 
+	return(str(des))
+
+
+def vevod(a, b):
+	print(a[0].ljust(8), a[1].ljust(8), a[2].ljust(8), a[3], sep='  ')
+	print(b[0], b[1], b[2], b[3], sep='  ')
+
+
+ip = input('Введите IP-сети: ')
+net = ip.split('/')[0].split('.')
+mask = ip.split('/')[1]
+# Network
+print('Network:')
+net_2 = [dvoih(int(num)) for num in net]
+vevod(net, net_2)
+print()
+# Mask
+print('Mask:')
+print('/' + mask)
+mask = int(mask)
+mask_str = '1' * mask + '0' * (32 - mask)
+mask_list = [mask_str[i * 8: (i + 1) * 8] for i in range(4)]
+mask_list_des = [des(i) for i in mask_list]
+vevod(mask_list_des, mask_list)
